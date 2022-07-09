a1999e8a902360edd75904b55e700419bcf4ea08
emonShimoul <emonshimoul@gmail.com>
2022-07-09 12:55:09 +0600

    build mongodb connection

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log("connected to mongodb");
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
